0b186900320eff7c989e0b6cc826236574b32703
maciecret <mercypulido4@gmail.com>
2023-01-17 02:47:19 +0800

    init

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -1,9 +1,12 @@
 import { BlogRoutingModule } from './blog-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+
 import { BlogListComponent } from './pages/blog-list/blog-list.component';
 import { BlogItemComponent } from './components/blog-item/blog-item.component';
 import { BlogFormComponent } from './pages/blog-form/blog-form.component';
+import {MatInputModule} from '@angular/material/input';
 
 
 
@@ -15,7 +18,8 @@ import { BlogFormComponent } from './pages/blog-form/blog-form.component';
   ],
   imports: [
     CommonModule,
-    BlogRoutingModule
+    BlogRoutingModule,
+    MatInputModule
   ]
 })
 export class BlogModule { 
